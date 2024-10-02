29e4f1101aa035a3b41c031c04876bc95624a487
Maverick <Mave3624@gmail.com>
2024-10-02 17:32:18 +0100

    added new features

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,22 @@
+
+    const btn = document.querySelector('.btn-Container')
+    const btn1 = document.querySelector('#btn1')
+    const btn2 = document.querySelector('#btn2')
+    const btn3 = document.querySelector('#btn3')
+    const show = document.querySelectorAll('.show')
+    const btn4 = document.createElement('button')
+    btn4.textContent = "Restart"
+
+    const Score = document.createElement('span')
+    Score.setAttribute("id", "sp2")
+    show[0].insertBefore(Score, sp3)
+    Score.textContent= " - — - "
+
+    show[1].innerHTML = 'Choose Between <br> Rock | Paper | Scissors'
+
+    
 // Computer Choice Function
-let getComputerChoice = function() {
+    let getComputerChoice = function() {
     let computer
     const random = Math.floor(Math.random()*10)+1;
     (random >= 0 && random <= 3) ? computer = "Rock":
@@ -7,102 +24,73 @@ let getComputerChoice = function() {
     computer = "Scissors";
     return computer
     }
-
-    const btn = document.querySelector('.btn-Container')
-    const btn1 = document.querySelector('#btn1')
-    const btn2 = document.querySelector('#btn2')
-    const btn3 = document.querySelector('#btn3')
-    const show = document.querySelectorAll('.show')
-
-    const human = document.createElement('span')
-    human.setAttribute('class', 'score1')
-    show[0].appendChild(human)
-    
-    const computer = document.createElement('span')
-    computer.setAttribute('class', 'score2')
-    show[0].appendChild(computer)
-    show[1].innerHTML = 'Choose Between <br> Rock | Paper | Scissors'
-    human.textContent = `YOUR SCORE| 0 —  `
-    computer.textContent = `0 |COMPUTER SCORE`    
-  
-    //Score 
-    
+ 
+//Score  
     let humanScore = 0
     let computerScore = 0
-    
-    btn1.addEventListener('click',() => {
-        Timer()
-            humanChoice = btn3.value;
-            computerChoice = getComputerChoice();
-            if (humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Rock") {
-                humanScore++
-                show[1].style.color = 'green';
-                show[1].textContent = `You won: ${humanChoice} beats ${computerChoice}` 
-                human.textContent = `YOUR SCORE| ${humanScore} —  `
-                
-            }
-             else if (humanChoice === computerChoice) {
-                show[1].textContent =  `It's a Tie`
-            }
-            else {
-                computerScore++
-                show[1].style.color = 'red';
-                show[1].textContent =  `Computer won: ${computerChoice} beats ${humanChoice}`
-                computer.textContent = `${computerScore} |COMPUTER SCORE`    
-            }
-            btn1.disabled = true
-            btn2.disabled = true
-            btn3.disabled = true
+
+function choiceChecker() {
+    show[2].textContent = ''
+    let computerChoice = getComputerChoice();
+    if (humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++
+        show[1].textContent = `${humanChoice}  beats  ${computerChoice}` 
+        Score.textContent = ` ${humanScore} — ${computerScore} ` 
+        show[1].style.color = 'green'
         }
+     else if (humanChoice === computerChoice) {
+        show[1].textContent =  `It's a Tie`
+    }
+    else {
+        computerScore++
+        show[1].textContent =  `${computerChoice}  beats  ${humanChoice}`
+        Score.textContent = ` ${humanScore} — ${computerScore} ` 
+        show[1].style.color = 'red'
+    }
+    if (humanScore < 5 && computerScore < 5) {
+        btn1.disabled = true
+        btn2.disabled = true
+        btn3.disabled = true
+    }
+    if (computerScore === 5 || humanScore === 5) {
+        show[2].textContent = 'Game Over'
+        show[1].textContent = ''
+        let result
+        if (humanScore > computerScore) result = `Congratulations, You Won`
+        if (computerScore > humanScore) result = `You Lost, Try Again`
+        show[1].textContent = result
+        btn.append(btn4)
+        btn1.remove()
+        btn2.remove()
+        btn3.remove()
+    }
+    else Timer()
+
+    btn4.addEventListener('click', () => {
+        humanScore = 0
+        computerScore = 0
+        show[2].textContent = ''
+        Score.textContent= " - — - "
+        show[1].style.color = 'black';
+        show[1].innerHTML = 'Choose Between <br> Rock | Paper | Scissors'
+        btn.append(btn1, btn2, btn3)
+        btn4.remove()
+    })
+}
+
+    btn1.addEventListener('click',() => {
+        humanChoice = btn1.value;
+            choiceChecker()
+    }
     )
     btn2.addEventListener('click',() => {
-        Timer()
-            humanChoice = btn3.value;
-            computerChoice = getComputerChoice();
-            if (humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Rock") {
-                humanScore++
-                show[1].style.color = 'green';
-                show[1].textContent = `You won: ${humanChoice} beats ${computerChoice}` 
-                human.textContent = `YOUR SCORE| ${humanScore} —  `
-                
-            }
-             else if (humanChoice === computerChoice) {
-                show[1].textContent =  `It's a Tie`
-            }
-            else {
-                computerScore++
-                show[1].style.color = 'red';
-                show[1].textContent =  `Computer won: ${computerChoice} beats ${humanChoice}`
-                computer.textContent = `${computerScore} |COMPUTER SCORE`    
-            }
-            btn1.disabled = true
-            btn2.disabled = true
-            btn3.disabled = true
+        humanChoice = btn2.value;
+            choiceChecker()
         }
     )
     btn3.addEventListener('click',() => {
-        Timer()
-            humanChoice = btn3.value;
-            computerChoice = getComputerChoice();
-            if (humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Rock") {
-                humanScore++
-                show[1].style.color = 'green';
-                show[1].textContent = `You won: ${humanChoice} beats ${computerChoice}` 
-                human.textContent = `YOUR SCORE| ${humanScore} —  `
-                
-            }
-             else if (humanChoice === computerChoice) {
-                show[1].textContent =  `It's a Tie`
-            }
-            else {
-                computerScore++
-                show[1].style.color = 'red';
-                show[1].textContent =  `Computer won: ${computerChoice} beats ${humanChoice}`
-                computer.textContent = `${computerScore} |COMPUTER SCORE`    
-            }
-            btn1.disabled = true
-            btn2.disabled = true
-            btn3.disabled = true
+        humanChoice = btn3.value;
+            choiceChecker()
         }
     )
  
@@ -113,6 +101,6 @@ function Timer() {
         btn3.disabled = false
         show[1].style.color = 'black';
         show[1].innerHTML = 'Choose Between <br> Rock | Paper | Scissors'
-        }, 800
+    }, 500
 )
 }
